2df9e975ed24afff4f429626f59c5e99622bd2eb
surya prakash reddy mereddy <spmereddy@unomaha.edu>
2017-03-04 17:34:37 -0600

    css changes

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,27 @@ require('laravel-elixir-vue-2');
  |
  */
 
-elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+elixir(function(mix) {
+    mix.copy(
+        'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
+        'resources/assets/js'
+    ).copy(
+        'vendor/bower_components/sweetalert/dist/sweetalert-dev.js',
+        'resources/assets/js/libs'
+    ).copy(
+        'vendor/bower_components/sweetalert/dist/sweetalert.css',
+        'resources/assets/css/libs'
+    );
+
+    mix.sass('app.scss',
+        'public/assets/css/app.css')
+        .styles([
+            'libs/sweetalert.css'
+        ],  'public/assets/css/libs.css')
+        .scripts([
+            'bootstrap.js'
+        ],  'public/assets/js/app.js')
+        .scripts([
+            'libs/sweetalert-dev.js'
+        ],  'public/assets/js/libs.js');
 });
