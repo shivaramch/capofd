607e6a1de3a40555b1b20eb90207d923e792992e
surya prakash reddy mereddy <spmereddy@unomaha.edu>
2017-03-04 18:17:32 -0600

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
-const elixir = require('laravel-elixir');
-
-require('laravel-elixir-vue-2');
+var elixir = require('laravel-elixir');
 
 /*
  |--------------------------------------------------------------------------
@@ -9,31 +7,11 @@ require('laravel-elixir-vue-2');
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
  | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
+ | file for our application, as well as publishing vendor resources.
  |
  */
 
 elixir(function(mix) {
-    mix.copy(
-        'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
-        'resources/assets/js'
-    ).copy(
-        'vendor/bower_components/sweetalert/dist/sweetalert-dev.js',
-        'resources/assets/js/libs'
-    ).copy(
-        'vendor/bower_components/sweetalert/dist/sweetalert.css',
-        'resources/assets/css/libs'
-    );
-
-    mix.sass('app.scss',
-        'public/assets/css/app.css')
-        .styles([
-            'libs/sweetalert.css'
-        ],  'public/assets/css/libs.css')
-        .scripts([
-            'bootstrap.js'
-        ],  'public/assets/js/app.js')
-        .scripts([
-            'libs/sweetalert-dev.js'
-        ],  'public/assets/js/libs.js');
+    mix.sass('app.scss');
+    mix.webpack('app.js');
 });
