3cc46e9857d991f8d7431ff67503e276de152291
KIRAN\kiran <vbadugu@unomaha.edu>
2017-03-10 18:40:21 -0600

    Form Upload Trait

---
+++ database/migrations/2017_03_10_061350_create_attachment_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttachmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachment', function (Blueprint $table) {
+            $table->increments('attachmentid');
+            $table->string('attachmentname', 256);
+            $table->string('createdby', 256);
+            $table->string('attachmenttype', 256);
+            $table->string('updatedby', 256);
+            $table->integer('ofd6cid')->nullable();
+            $table->integer('ofd6id')->nullable();
+            $table->integer('ofd6bid')->nullable();
+            $table->integer('ofd6aid')->nullable();
+
+            $table->timestamps();
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('attachment');
+    }
+}
