73e89aa6cbc975df9f3f39208ed668275e3123f0
KIRAN\kiran <vbadugu@unomaha.edu>
2017-03-12 13:15:12 -0500

    Injury Update Rules

--- app/Http/Requests/UpdateInjuriesRequest.php
+++ app/Http/Requests/UpdateInjuriesRequest.php
@@ -24,21 +24,19 @@ class UpdateInjuriesRequest extends FormRequest
     public function rules()
     {
         return [
-            'reportnum' => 'required|string:accidents,reportnum,'. $this->route('accident'),
-            'injurydate' => 'required|date:accidents,injurydate,'. $this->route('accident'),
-            'createdate' => 'required|date:accidents,createdate,'. $this->route('accident'),
-            'injuredemployeename' => 'required|string:accidents,injuredemployeename,'. $this->route('accident'),
-            'injuredemployeeid' => 'required|integer:accidents,injuredemployeeid,' . $this->route('accident'),
-            'injuredemployeename' => 'required|string:accidents,injuredemployeename,'. $this->route('accident'),
-            'assignmentinjury' => 'required|string:accidents,assignmentinjury,'. $this->route('accident'),
-            'corvelid' => 'required|integer:accidents,corvelid,' . $this->route('accident'),
-            'captainid' => 'required|integer:accidents,captainid'. $this->route('accident'),
-            'battalionchiefid' => 'required|integer:accidents,battalionchiefid'. $this->route('accident'),
-            'aconduty' => 'required|integer:accidents,aconduty'. $this->route('accident'),
-            'shift' => 'required|string:accidents,shift,'. $this->route('accident'),
-            'frmsincidentnum' => 'required|string:accidents,frmsincidentnum'. $this->route('accident'),
-            'policeofficercompletesign' => 'required|string:accidents,policeofficercompletesign'. $this->route('accident'),
-            'callsupervisor' => 'required|string:accidents,callsupervisor'. $this->route('accident')
+
+            'injurydate' => 'required|date:injury,injurydate,'. $this->route('injury'),
+            'createdate' => 'required|date:injury,createdate,'. $this->route('injury'),
+            'injuredemployeename' => 'required|string:injury,injuredemployeename,'. $this->route('injury'),
+            'injuredemployeeid' => 'required|integer:injury,injuredemployeeid,' . $this->route('injury'),
+
+            'assignmentinjury' => 'required|string:injury,assignmentinjury,'. $this->route('injury'),
+            'corvelid' => 'required|integer:injury,corvelid,' . $this->route('injury'),
+            'captainid' => 'required|integer:injury,captainid'. $this->route('injury'),
+            'battalionchiefid' => 'required|integer:injury,battalionchiefid'. $this->route('injury'),
+            'aconduty' => 'required|integer:injury,aconduty'. $this->route('injury'),
+            'shift' => 'required|string:injury,shift,'. $this->route('injury'),
+            'frmsincidentnum' => 'required|string:injury,frmsincidentnum'. $this->route('injury')
         ];
     }
 }
