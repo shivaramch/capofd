32d2f903ad95cfc8c67e4d3f49f126f765d43248
KIRAN\kiran <vbadugu@unomaha.edu>
2017-03-13 02:16:57 -0500

    Injury Rules with required checkbox rule

--- app/Http/Requests/UpdateInjuriesRequest.php
+++ app/Http/Requests/UpdateInjuriesRequest.php
@@ -24,19 +24,22 @@ class UpdateInjuriesRequest extends FormRequest
     public function rules()
     {
         return [
-
+            'reportnum' => 'required|integer:injury,reportnum,'. $this->route('injury'),
             'injurydate' => 'required|date:injury,injurydate,'. $this->route('injury'),
             'createdate' => 'required|date:injury,createdate,'. $this->route('injury'),
             'injuredemployeename' => 'required|string:injury,injuredemployeename,'. $this->route('injury'),
             'injuredemployeeid' => 'required|integer:injury,injuredemployeeid,' . $this->route('injury'),
-
             'assignmentinjury' => 'required|string:injury,assignmentinjury,'. $this->route('injury'),
             'corvelid' => 'required|integer:injury,corvelid,' . $this->route('injury'),
             'captainid' => 'required|integer:injury,captainid'. $this->route('injury'),
             'battalionchiefid' => 'required|integer:injury,battalionchiefid'. $this->route('injury'),
             'aconduty' => 'required|integer:injury,aconduty'. $this->route('injury'),
+            'documentworkforce' => 'required',
+            'documentoperationalday' => 'required',
             'shift' => 'required|string:injury,shift,'. $this->route('injury'),
-            'frmsincidentnum' => 'required|string:injury,frmsincidentnum'. $this->route('injury')
+            'frmsincidentnum' => 'required|string:injury,frmsincidentnum'. $this->route('injury'),
+            'policeofficercompletesign' => 'required',
+            'callsupervisor' => 'required',
         ];
     }
 }
