e94518f196e41591e5d34d1adc0c48c97dcd3f21
surya prakash reddy mereddy <spmereddy@unomaha.edu>
2017-03-18 16:48:08 -0500

    Index for all forms

--- resources/views/biologicals/index.blade.php
+++ resources/views/biologicals/index.blade.php
@@ -20,7 +20,11 @@
         </div>
     </div>
 
-    @foreach($biologicals as $biological)
+    @if(count($biologicals) > 0)
+
+        @foreach($biologicals as $biological)
+        @endforeach
+
         @if($biological->employeeid == Auth::user()->id)
             <div class="panel panel-default panel-shadow " hidden>
                 <div class="panel-heading">
@@ -45,34 +49,33 @@
                         </tr>
                         </thead>
                         <tbody>
-                        @if(count($biologicals) > 0)
-
-                            <tr>
-                                <td>{{ $biological->ofd6bid }}</td>
-                                <td>{{ $biological->dateofexposure }}</td>
-                                <td>{{ $biological->assignmentbiological }}</td>
-                                <td>{{ $biological->status }}</td>
-                                <td>
-                                    <div>
-                                        <a href="{{ route('biologicals.show',[$biological->ofd6bid]) }}"
-                                           class="btn btn-xs btn-info btn-block"><i
-                                                    class="fa fa-eye" aria-hidden="true"></i> VIEW</a>
-                                        <a href="{{ route('biologicals.edit',[$biological->ofd6bid]) }}"
-                                           class="btn btn-xs btn-warning btn-block"><i class="fa fa-pencil-square-o"
-                                                                                       aria-hidden="true"></i> EDIT</a>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endif
+                        @foreach($biologicals as $biological)
+                            @if($biological->employeeid == Auth::user()->id)
+                                <tr>
+                                    <td>{{ $biological->ofd6bid }}</td>
+                                    <td>{{ $biological->dateofexposure }}</td>
+                                    <td>{{ $biological->assignmentbiological }}</td>
+                                    <td>{{ $biological->status }}</td>
+                                    <td>
+                                        <div>
+                                            <a href="{{ route('biologicals.show',[$biological->ofd6bid]) }}"
+                                               class="btn btn-xs btn-info btn-block"><i
+                                                        class="fa fa-eye" aria-hidden="true"></i> VIEW</a>
+                                            <a href="{{ route('biologicals.edit',[$biological->ofd6bid]) }}"
+                                               class="btn btn-xs btn-warning btn-block"><i class="fa fa-pencil-square-o"
+                                                                                           aria-hidden="true"></i> EDIT</a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endif
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
         @endif
 
-
-
-        @if($biological->primaryidconumber == Auth::user()->id && $biological->applicationstatus == 2)
+        @if($biological->primaryidconumber == Auth::user()->id )
             <div class="panel panel-default panel-shadow " hidden>
                 <div class="panel-heading">
                     In your Queue For Approval
@@ -87,40 +90,39 @@
                            data-show-columns="true"
                            id="table">
                         <thead>
-                                <tr>
-                                    <th data-sortable="true">OFD 6B ID</th>
-                                    <th data-sortable="true">Date of Exposure</th>
-                                    <th data-sortable="true">Assignment</th>
-                                    <th data-sortable="true">Status</th>
-                                    <th data-switchable="false" data-searchable="false" data-sortable="false">Action
-                                    </th>
-                                </tr>
+                        <tr>
+                            <th data-sortable="true">OFD 6B ID</th>
+                            <th data-sortable="true">Date of Exposure</th>
+                            <th data-sortable="true">Assignment</th>
+                            <th data-sortable="true">Status</th>
+                            <th data-switchable="false" data-searchable="false" data-sortable="false">Action
+                            </th>
+                        </tr>
                         </thead>
                         <tbody>
-                        @if(count($biologicals) > 0)
-                            @if($biological->applicationstatus == 2)
+                        @foreach($biologicals as $biological)
+                            @if($biological->primaryidconumber == Auth::user()->id && $biological->applicationstatus == 2)
                                 <tr>
-                            <td>{{ $biological->ofd6bid }}</td>
-                            <td>{{ $biological->dateofexposure }}</td>
-                            <td>{{ $biological->assignmentbiological }}</td>
-                            <td>{{ $biological->status }}</td>
-                            <td>
-                                <div>
-                                    <a href="{{ route('biologicals.show',[$biological->ofd6bid]) }}"
-                                       class="btn btn-xs btn-info btn-block"><i
-                                                class="fa fa-eye" aria-hidden="true"></i> VIEW</a>
-                                </div>
-                            </td>
-                        </tr>
-                        @endif
-                        @endif
+                                    <td>{{ $biological->ofd6bid }}</td>
+                                    <td>{{ $biological->dateofexposure }}</td>
+                                    <td>{{ $biological->assignmentbiological }}</td>
+                                    <td>{{ $biological->status }}</td>
+                                    <td>
+                                        <div>
+                                            <a href="{{ route('biologicals.show',[$biological->ofd6bid]) }}"
+                                               class="btn btn-xs btn-info btn-block"><i
+                                                        class="fa fa-eye" aria-hidden="true"></i> VIEW</a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endif
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
         @endif
-
-    @endforeach
+    @endif
 
 @stop
 @section('javascript')
