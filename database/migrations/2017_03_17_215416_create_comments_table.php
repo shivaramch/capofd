caf34d1d91864560d634e8bc39026a5fee055a70
Varun Hegde <vhegde@unomaha.edu>
2017-03-18 16:10:13 -0500

    Migration changes

---
+++ database/migrations/2017_03_17_215416_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('commentid');
+            $table->text('comment')->nullable();
+            $table->string('createdby', 256)->nullable();
+            $table->string('applicationtype', 256)->nullable();
+            $table->integer('applicationid')->nullable();
+            $table->string('role')->nullable();
+            $table->ipAddress('ip_address', 45)->nullable();
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,26 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(StatusTableSeeder::class);
+    }
+}
+
+class StatusTableSeeder extends Seeder
+{
+
+
+    public function run()
+    {
+        DB::table('status')->insert(['statustype' => 'Draft', ]);
+        DB::table('status')->insert(['statustype' => 'Application under Captain',]);
+        DB::table('status')->insert(['statustype' => 'Application under Batallion Chief',]);
+        DB::table('status')->insert(['statustype' =>'Application under Assistant Chief',]);
+        DB::table('status')->insert(['statustype' =>'Rejected',]);
+        DB::table('status')->insert(['statustype' =>'Approved',]);
+
     }
 }
